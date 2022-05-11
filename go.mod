d259237cfbd1f264e9ea50401394705aa81d3114
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-11 22:03:55 +0700

    Menambah dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/CoderJava/app-hello-world
+
+go 1.18
+
+require github.com/CoderJava/go-hello-world v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_hello_world "github.com/CoderJava/go-hello-world"
+)
+
+func main() {
+	fmt.Println(go_hello_world.SayHello())
+}
